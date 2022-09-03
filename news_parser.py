28a9c08a8f5ccd1eec293321bba1f81d52cd949d
sevmarc <marton.sevella@gmail.com>
2022-09-03 23:04:02 +0200

    Big update, more functionality
    - pythonized, useful classes added
    - some optimization would be great

--- news_parser.py
+++ news_parser.py
@@ -1,31 +1,106 @@
+from typing import Optional
 import requests
-# import pprint
 from bs4 import BeautifulSoup
-import re
-# import html2text
+from grappa import expect
+from http import HTTPStatus
+from urllib.parse import urljoin
 
 
-def get_headline(line, class_text):
-    match = re.search(r'(<span class="%s">)(.*)(</span>)' % class_text, line)
-    if match:
-        return match.groups()[1]
-    else:
-        return None
+HeadlineDictType = dict[str, list[str]]
+
+
+class HeadlineList():
+    """ Used for printing list of headlines """
+
+    def __init__(self, headline_list: list[str]):
+        self.headlines = headline_list
+
+    def __str__(self) -> str:
+        return "".join([f"- {headline}\n" for headline in self.headlines]).rstrip('/n')
+
+    def __repr__(self) -> str:
+        return self.__str__
+
+
+class HeadlineDict():
+    """ Used for printing a dictionary of header - headline list values """
+
+    def __init__(self, headline_dict: HeadlineDictType):
+        self.headline_dict = headline_dict
+
+    def __str__(self) -> str:
+        text = ""
+        for header, headlines in self.headline_dict.items():
+            text += f"{header}\t" + "-" * 20 + '\n'
+            text += str(headlines)
+        return text
+
+    def __repr__(self) -> str:
+        return self.__str__
 
 
 class news():
-    def __init__(self, URL_BASE, SECTION):
-        self.URL_BASE = URL_BASE
-        self.SECTION = SECTION
+    def __init__(self, url_base: str = "https://edition.cnn.com", filtering: Optional[bool] = False):
+        if not url_base.startswith("http"):
+            url_base = "https://" + url_base
+        self.URL_BASE = url_base
+        self.URL = self.URL_BASE  # this can be overwritten with self.set_section()
+        self.section_filter = ["world", "europe", "business"] if filtering else []
 
-        URL = URL_BASE + SECTION
-        page = requests.get(URL)
-        # pprint(page.content)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        # print(soup.prettify())
-        # print(soup.get_text())
-        print('========================================================')
-        for headline in soup.find_all('span'):
-            hl = get_headline(str(headline), 'cd__headline-text')
-            if hl:
-                print(hl)
+    @property
+    def page(self) -> requests.Response:
+        try:
+            response = requests.get(self.URL)
+            expect(response.status_code).to.equal(HTTPStatus.OK)
+            return response
+        except ConnectionError:
+            print(f"Connection refused: {self.URL = }")
+            return None
+
+    @property
+    def soup(self) -> "BeautifulSoup":
+        return BeautifulSoup(self.page.content, 'html.parser')
+
+    @property
+    def section_list(self) -> list[str]:
+        return [header_tag["href"] for header_tag in self.soup.find_all('a', attrs={"data-analytics": "footer"}) if header_tag["href"].startswith('/')]
+
+    def set_section(self, section: str) -> str:
+        self.URL = urljoin(self.URL_BASE, section)
+
+    def get_headlines(self) -> HeadlineList:
+        return HeadlineList([headline.text for headline in self.soup.find_all("span", {"class": "cd__headline-text"})])
+
+    @property
+    def section(self) -> str:
+        return self.URL.rsplit('.com/')[-1]
+
+    @property
+    def headline_dict(self) -> HeadlineDict:
+        headline_dict = {}
+        
+        self.set_section("")  # homepage
+        headline_dict.update({"homepage": self.get_headlines()})
+        
+        for section in self.section_list:
+            self.set_section(section)
+            if not self.section_filter or self.section in self.section_filter:
+                hl = self.get_headlines()
+                if hl:
+                    headline_dict.update({self.section: hl})
+        return HeadlineDict(headline_dict)
+
+    @property
+    def all_headlines(self) -> HeadlineList:
+        high_list = self.headline_dict.headline_dict.values()
+        return HeadlineList([item for sublist in high_list for item in sublist.headlines])
+
+    def search_for_keyword(self, keyword: str) -> HeadlineList:
+        print(f"Results for '{keyword}': ")
+        return HeadlineList(list(filter(lambda x: keyword.lower() in x.lower(), self.all_headlines.headlines)))
+
+
+if __name__ == '__main__':
+    cnn = news(filtering=True)
+    print(cnn.headline_dict)
+    print(cnn.search_for_keyword("Russia"))
