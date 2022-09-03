89d954e5d74a14b214de34d9605130367fce41ac
sevmarc <marton.sevella@gmail.com>
2022-09-03 23:17:28 +0200

    Basic update, to match new version of news_parser
    - TODO: extend functionality with search
    - optimize

--- trial.py
+++ trial.py
@@ -2,34 +2,44 @@ import PySimpleGUI as sg
 from news_parser import news
 
 
-sg.theme('DarkTanBlue')  # Add a touch of color
-# All the stuff inside your window.
-layout = [[sg.Text('This simple GUI is only for experimentation!')],
-          [sg.Text('Enter CNN Section: '), sg.InputText()],
-          [sg.Text('Open File with path:'), sg.InputText()],
-          [sg.Button('Open file'), sg.Button('NEWS'), sg.Button('Cancel')]]
+def gui():
+    cnn = news(filtering=True)
+    header_list = cnn.section_list
+    
+    sg.theme('DarkTanBlue')  # Add a touch of color
+    # All the stuff inside your window.
+    layout = [[sg.Text('This simple GUI is only for experimentation!')],
+            [sg.Text('Enter CNN Section: '), sg.Combo(header_list)],
+            [sg.Text('Open File with path:'), sg.InputText()],
+            [sg.Button('Open file'), sg.Button('NEWS'), sg.Button('Cancel')]]
 
-# Create the Window
-window = sg.Window('Parsing CNN News - playground', layout)
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
+    # Create the Window
+    window = sg.Window('Parsing CNN News - playground', layout)
+    
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
 
-    if event == sg.WIN_CLOSED or event == 'Cancel':
-        # if user closes window or clicks cancel
-        break
+        if event == sg.WIN_CLOSED or event == 'Cancel':
+            # if user closes window or clicks cancel
+            break
 
-    if event == 'Open file':
-        try:
-            f_in = open(values[1], 'r')
-            for line in f_in:
-                print(line)
-        except FileNotFoundError:
-            print('File was not found!')
+        if event == 'Open file':
+            try:
+                f_in = open(values[1], 'r')
+                for line in f_in:
+                    print(line)
+            except FileNotFoundError:
+                print('File was not found!')
 
-    if event == 'NEWS':
-        URL_BASE = 'https://edition.cnn.com'
-        SECTION = '/' + values[0]
-        news(URL_BASE, SECTION)
+        if event == 'NEWS':
+            section = values[0]
+            cnn.set_section(section)
+            print(cnn.get_headlines())
+            
+    window.close()
 
-window.close()
+
+if __name__ == '__main__':
+    gui()
+    
