3bee97673a712fd183cac75ccc416fc4b5654c48
sevmarc <marton.sevella@gmail.com>
2022-09-04 19:38:37 +0200

    Update
    - Add filtering checkbox
    - Add search fucntionality

--- trial.py
+++ trial.py
@@ -10,8 +10,11 @@ def gui():
     # All the stuff inside your window.
     layout = [[sg.Text('This simple GUI is only for experimentation!')],
             [sg.Text('Enter CNN Section: '), sg.Combo(header_list)],
-            [sg.Text('Open File with path:'), sg.InputText()],
-            [sg.Button('Open file'), sg.Button('NEWS'), sg.Button('Cancel')]]
+            [sg.Text('Search for keyword: '), sg.InputText()],
+            [sg.Text('Open File with path: '), sg.InputText()],
+            [],
+            [sg.Button('Open file'), sg.Button('NEWS'), sg.Button("Search"), sg.Button('Cancel')],
+            [sg.Checkbox("Filtering sections: ", default=True, key="filtering")]]
 
     # Create the Window
     window = sg.Window('Parsing CNN News - playground', layout)
@@ -19,14 +22,14 @@ def gui():
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
-
+        
         if event == sg.WIN_CLOSED or event == 'Cancel':
             # if user closes window or clicks cancel
             break
 
         if event == 'Open file':
             try:
-                f_in = open(values[1], 'r')
+                f_in = open(values[3], 'r')
                 for line in f_in:
                     print(line)
             except FileNotFoundError:
@@ -36,6 +39,14 @@ def gui():
             section = values[0]
             cnn.set_section(section)
             print(cnn.get_headlines())
+        
+        if event == "Search":
+            print(cnn.search_for_keyword(values[1]))
+            
+        if values["filtering"]:
+            cnn.set_filtering(True)
+        else:
+            cnn.set_filtering(False)
             
     window.close()
 
